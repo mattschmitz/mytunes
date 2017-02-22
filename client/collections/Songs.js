88d04ee02a0864e649fd949d8865b0674de90188
mattschmitz <mattschmitz9@gmail.com>
2017-02-21 16:18:56 -0800

    passes BMR - re-renders library on successful get requests

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -6,13 +6,15 @@ var Songs = Backbone.Collection.extend({
   initialize: function () {
     var thisSongs = this;
     $.ajax({
-      url: 'http://parse.sfm8.hackreactor.com/mytunes/classes/songs.json',
+      url: 'http://parse.sfm8.hackreactor.com/mytunes/classes/songs',
       type: 'GET',
       dataType: 'json',
-      contentType: 'application/json',
       success: function (data) {
         // console.log(data.results);
         thisSongs.add(data.results);
+
+        //transmit success event.
+        thisSongs.trigger('newSongs', this);
 
         // thisSongs.forEach(function(model) {
         //   console.log(model.get('url'));
--- client/views/LibraryView.js
+++ client/views/LibraryView.js
@@ -5,6 +5,12 @@ var LibraryView = Backbone.View.extend({
 
   initialize: function() {
     this.render();
+    //listen for this.collection.on('sucess of get request') - this.render();
+    this.collection.on('newSongs', this.newSongsHandler, this);
+  },
+
+  newSongsHandler: function() {
+    this.render();
   },
 
   render: function() {
